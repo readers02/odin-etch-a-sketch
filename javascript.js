7ace5e2ed17166496d0f8dc12acae0a9762b61ef
readers02 <scott.reader@rogers.com>
2026-03-06 13:55:34 -0500

    Implement clear and resize button functionality

--- javascript.js
+++ javascript.js
@@ -3,10 +3,37 @@ function setDrawArea(size){
     for(let i = 0; i<(size*size); i++){
         const square = document.createElement("div");
         square.className = "drawSquare";
-        const squareSize = 100/16
+        const squareSize = 100/size
         square.style.height=squareSize + "%";
         square.style.width=squareSize + "%";
+        square.style.backgroundColor = ("#" + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0'));
         drawArea.appendChild(square);
+    }
+}
+
+function clearDrawArea(){
+    const drawSquareList = document.querySelectorAll(".drawSquare");
+    drawSquareList.forEach(square => {
+        square.style.backgroundColor = "#ffffff";
+    });
+}
+
+function resizeDrawArea() {
+    let sizeString = prompt("Enter the number of squares you want along the length of the drawing area. (Must be positive and no greater than 100)");
+    let size = null;
+    try {
+        size = parseInt(sizeString,10);
+    } catch (error) {
+        alert("Please enter a number.")
+    }
+    const drawSquareList = document.querySelectorAll(".drawSquare");
+    if (!isNaN(size) && size < 101 && size > 0) {
+        drawSquareList.forEach(square => {
+            square.remove();
+        });
+        setDrawArea(size);
+    }else{
+        alert("Something went wrong, please try again.")
     }
 }
 
